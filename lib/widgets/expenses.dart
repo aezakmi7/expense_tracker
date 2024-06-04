d5e208849048199d5a76838271be1284bd2d4bbb
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-06-04 16:16:37 -0400

    tuesday

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,5 @@
 import 'package:expense_tracker/datamodel/expense.dart';
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses-list/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
